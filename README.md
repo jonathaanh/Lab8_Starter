bbbc6bd1eecc282212fbf4ab6146334e3ef126e1
Jonathan Hsu <johnnhsu1031@gmail.com>
2022-11-19 14:13:43 -0800

    Lab Complete

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # Lab 8 - Starter
+## Jonathan Hsu
+
+### Check Your Understanding Questions
+1. The automated tests would fit within a Github action that runs whenever code is pushed. Automated tests are designed to be performed automatically when some action is triggered. In our project pipeline, the easiest way to implement this is when code is pushed to Github. 
+2. No, end to end testing is designed for high level functionality rather than individual functions. To test individual functions we can use unit tests. 
+3. No, the message feature of a application is high level, so it would be easier to test that functionality with an end to end test.
+4. Yes, I could setup a simple unit test to check that the user cannot enter more than 80 messages into the messaging textbox. 
--- __tests__/unit.test.js
+++ __tests__/unit.test.js
@@ -3,3 +3,34 @@
 const functions = require('../code-to-unit-test/unit-test-me.js');
 
 // TODO - Part 2
+test('isPhoneNumber', () => {
+	expect(functions.isPhoneNumber('(858)888-8888')).toBe(true);
+	expect(functions.isPhoneNumber('858-555-2220')).toBe(true);
+	expect(functions.isPhoneNumber('(858)888-88')).toBe(false);
+	expect(functions.isPhoneNumber('(858)88812313')).toBe(false);
+});
+test('isEmail', () => {
+	expect(functions.isEmail('jihsu@ucsd.edu')).toBe(true);
+	expect(functions.isEmail('jonathanihsu@gmail.com')).toBe(true);
+	expect(functions.isEmail('adfal.cd@')).toBe(false);
+	expect(functions.isEmail('dfad-cda.dd&&')).toBe(false);
+});
+test('isStrongPassword', () => {
+	expect(functions.isStrongPassword('Jjjlddf')).toBe(true);
+	expect(functions.isStrongPassword('jnn2340fd')).toBe(true);
+	expect(functions.isStrongPassword('&*#&*$&*')).toBe(false);
+	expect(functions.isStrongPassword('jfahdfjadfhakfhkda')).toBe(false);
+});
+test('isDate', () => {
+	expect(functions.isDate('10/31/2002')).toBe(true);
+	expect(functions.isDate('5/6/2012')).toBe(true);
+	expect(functions.isDate('32/1231/123')).toBe(false);
+	expect(functions.isDate('1341/413/13')).toBe(false);
+});
+
+test('isHexColor', () => {
+	expect(functions.isHexColor('#574240')).toBe(true);
+	expect(functions.isHexColor('#ffffff')).toBe(true);
+	expect(functions.isHexColor('#fadf23rfq')).toBe(false);
+	expect(functions.isHexColor('#f')).toBe(false);
+});
